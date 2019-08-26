96c7eff4dfe1dedf7e805e0df4bfe5acd4d679d9
wellfrog <wellfrog16@gmail.com>
2019-08-27 02:59:20 +0800

    更新loading

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -1,11 +1,41 @@
 import React from 'react';
-import { Spin } from 'antd';
-import style from './index.module.scss';
+import { LoadingComponentProps } from 'react-loadable';
+import NProgress from 'nprogress';
+// import PropTypes from 'prop-types';
+// import axios, { AxiosInstance } from 'axios';
+// import { DatePicker } from 'antd';
 
-const com: React.FC = () => (
-    <div className={style.main}>
-        <Spin />
-    </div>
-);
+NProgress.configure({ showSpinner: false });
 
-export default com;
+export default class Com extends React.Component<LoadingComponentProps> {
+    componentDidMount() {
+        NProgress.start();
+    }
+
+    componentWillUnmount() {
+        NProgress.done();
+    }
+    // console.log(props);
+    // const { error, timedOut, pastDelay } = props;
+    // if (error) {
+    //     NProgress.done();
+    //     // return <div>Error! <button onClick={props.retry}>Retry</button></div>;
+    // } else if (timedOut) {
+    //     NProgress.done();
+    //     // return <div>Taking a long time... <button onClick={props.retry}>Retry</button></div>;
+    // } else if (pastDelay) {
+    //     NProgress.start();
+    //     // return <div>Loading...</div>;
+    // } else {
+    //     NProgress.done();
+    //     // return null;
+    // }
+
+    render() {
+        return (
+            <div className="main-wrapper">
+                loading
+            </div>
+        );
+    }
+}
