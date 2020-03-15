6d9faaca9835cb332ec4eb506ee41dc094a62c13
wellfrog <wellfrog16@gmail.com>
2020-03-15 17:45:46 +0800

    修复示例

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 // import Page1 from './page1';
 // import Page2 from './page2';
 import Layout from './pages/layout';
@@ -7,15 +7,17 @@ import Login from './pages/login';
 
 // const a = React.lazy(() => import('@/pages/research/table'));
 
-const app: React.FC = () => (
+const app = () => (
     <HashRouter>
-        <Route path="/" component={Layout} />
+        <Switch>
+            <Route path="/login" exact component={Login} />
+            <Route path="/" component={Layout} />
+        </Switch>
 
         {/* <Route exact={true} onEnter={test()} component={Layout}>
             <Route exact={true} path="/layout/page2" component={Page2}/>
             <Route exact={true} path="/layout/page1" component={Page1}/>
         </Route> */}
-        <Route path="/login" component={Login} />
     </HashRouter>
 );
 
